127997575a4fd7e8114bc0e58592dc43fab8cd3d
Armstrong <SArmstrong@urban.org>
2016-06-14 15:40:31 -0400

    Added to subset module: initial cache of data, dim(data) check, select() section, updated filter() section, SQL statement e.g. given for select() and filter(), section on arrange() started

---
+++ ss1-summary-statistics.R
@@ -0,0 +1,19 @@
+#########################################################
+### Social Science Module 1: Basic Summary Statistics ###
+#########################################################
+
+library(SparkR)
+
+## Initiate SparkContext:
+sc <- sparkR.init(sparkEnvir=list(spark.executor.memory="2g", 
+                                  spark.driver.memory="1g",
+                                  spark.driver.maxResultSize="1g")
+                  ,sparkPackages="com.databricks:spark-csv_2.11:1.4.0") ## Load CSV Spark Package
+## AWS EMR is using Spark 2.11 so we need the associated version of spark-csv: http://spark-packages.org/package/databricks/spark-csv
+## Define Spark executor memory, as well as driver memory and maxResultSize according to cluster configuration
+
+## Initiate SparkRSQL:
+sqlContext <- sparkRSQL.init(sc)
+
+## Read in example HFPC data (quarterly performance data from XXXX) from AWS S3:
+data <- read.df(sqlContext, "s3://ui-hfpc/hfpc_ex_par", header='false', inferSchema='true')
