1710d1222c1464d9949f56f5c5abb71da7612964
Armstrong <SArmstrong@urban.org>
2016-07-01 12:47:53 -0400

    Updated filter section of subsetting .R file

--- R/subsetting.R
+++ R/subsetting.R
@@ -44,25 +44,30 @@ f1 <- filter(df, df$servicer_name == "JP MORGAN CHASE BANK, NA" | df$servicer_na
 f2 <- filter(df, "servicer_name = 'JP MORGAN CHASE BANK, NA' or servicer_name = 'JPMORGAN CHASE BANK, NA' or servicer_name = 'JPMORGAN CHASE BANK, NATIONAL ASSOCIATION'")
 (n2 <- nrow(f2))
 
+## Or, alternatively, in syntax similar to how we can subset by row in base R:
+
+f3 <- df[df$servicer_name == "JP MORGAN CHASE BANK, NA" | df$servicer_name == "JPMORGAN CHASE BANK, NA" | df$servicer_name == "JPMORGAN CHASE BANK, NATIONAL ASSOCIATION",]
+(n3 <- nrow(f3))
+
 ## Subset the DF as 'f3', which includes only those loans for which the servicer name is known, i.e. servicer name is not equal to an empty entry or listed as "other".
 ## Execute 'filter()' with an 'is not equal to' logical condition (!=) and an 'and' logical operation (&):
-f3 <- filter(df, df$servicer_name != "OTHER" & df$servicer_name != "")
-(n3 <- nrow(f3))
+f4 <- filter(df, df$servicer_name != "OTHER" & df$servicer_name != "")
+(n4 <- nrow(f4))
 
 ## Subset DF as 'f4', which includes only loans for which the 'loan_age', i.e. the number of calendar months since the first full month the mortgage loan accrues interest, is
 ## greater than 5 years (60 calendar months); execute 'filter()' with an 'is greater than' logical condition (>):
-f4 <- filter(df, df$loan_age > 60)
-(n4 <- nrow(f4))
+f5 <- filter(df, df$loan_age > 60)
+(n5 <- nrow(f5))
 
 ## Subset DF as 'f5', which includes only loans for which the 'loan_age' is greater than, or equal to, 10 years (120 calendar months); execute 'filter()' with an 'is greater
 ## than or equal to' logical condition (>=):
-f5 <- filter(df, df$loan_age >= 120)
-(n5 <- nrow(f5))
+f6 <- filter(df, df$loan_age >= 120)
+(n6 <- nrow(f6))
 
 ## An alias for `filter` is `where`, which presents diction that is often much more intuitive, particularly when `where` is embedded in a complex statement. For example, the following expression can be read as "aggregate and return the mean loan age and count values for observations in `df` where loan age is less than 60 months"
 
-f6 <- agg(groupBy(where(df, df$loan_age < 60), where(df, df$loan_age < 60)$servicer_name), loan_age_avg = avg(where(df, df$loan_age < 60)$loan_age), count = n(where(df, df$loan_age < 60)$loan_age))
-head(f6)
+f7 <- agg(groupBy(where(df, df$loan_age < 60), where(df, df$loan_age < 60)$servicer_name), loan_age_avg = avg(where(df, df$loan_age < 60)$loan_age), count = n(where(df, df$loan_age < 60)$loan_age))
+head(f7)
 
 #############################################################################################################################
 ## (2) Subset DF by column, i.e. select the columns of a DF according to a given condition, using the operation `select`: ###
@@ -71,17 +76,17 @@ head(f6)
 ## 
 
 
-## Not run: 
+
 ##D   # Columns can be selected using `[[` and `[`
 ##D   df[[2]] == df[["age"]]
 ##D   df[,2] == df[,"age"]
 ##D   df[,c("name", "age")]
-##D   # Or to filter rows
-##D   df[df$age > 20,]
+
+
+
 ##D   # DataFrame can be subset on both rows and Columns
 ##D   df[df$name == "Smith", c(1,2)]
 ##D   df[df$age %in% c(19, 30), 1:2]
 ##D   subset(df, df$age %in% c(19, 30), 1:2)
 ##D   subset(df, df$age %in% c(19), select = c(1,2))
 ##D   subset(df, select = c(1,2))
-## End(Not run)
