b7e2fe1a17f4a895a66d94f9359641887c7b1cbf
Armstrong <SArmstrong@urban.org>
2016-07-06 15:08:27 -0400

    Completed sections on missing data conditionals and missing conditionals with grouped data in missing-data.R tutorial

--- R/missing-data.R
+++ R/missing-data.R
@@ -14,12 +14,31 @@ sc <- sparkR.init(sparkEnvir=list(spark.executor.memory="2g",
 
 sqlContext <- sparkRSQL.init(sc)
 
-## Read in loan performance example data:
+### Specify null values when loading data:
 
-df <- read.df(sqlContext, "s3://sparkr-tutorials/hfpc_ex", header='false', inferSchema='true', nullValue='')
+## Note that we now include the `nullValue` option in the `read.df` transformation below. By setting `nullValue` equal to an empty string in `read.df`, we direct `read.df` and the `sqlContext` to interpret empty string entries as being equal to a null value. Therefore, any DF entries matching this string will be set as nulls in `df`:
+
+df <- read.df(sqlContext, "s3://sparkr-tutorials/hfpc_ex", header="false", inferSchema="true", nullValue="")
 cache(df)
 
-# nullValue (in read.df)
+## We can replace this empty string with any string that we know indicates a null entry in the dataset, i.e. with `nullValue="<string>"`.
+
+
+### Condition on null entries:
+
+## We saw in the subsetting tutorial how to subset a DF based on a conditional statement. We can extend this reasoning in order to identify missing data in a DF and to explore the distribution of missing data within a DF. Conditional operations for missing data in SparkR are `isNull`, `isNaN` and `isNotNull`, which can be used to condition on missing or NaN entries of a DF column. Below, we count the number of missing entries in `"loan_age"` and in `"mths_remng"`. We can see below that there are no missing or NaN entries in `"loan_age"`. Note that the `isNull` and `isNaN` count results differ for `"mths_remng"` - while there are missing values in `"mths_remng"`, there are no NaN entries (entires that are "not a number").
+count(where(df, isNull(df$loan_age)))
+count(where(df, isNull(df$mths_remng)))
+
+count(where(df, isNaN(df$loan_age)))
+count(where(df, isNaN(df$mths_remng)))
+
+## We can also condition on missing data when aggregating over grouped data in order to see how missing data is distributed over a categorical variable within our data. In order to view the distribution of missing `"mths_remng"` observations over `"servicer_name"`, we (1) create the DF `df_MRnull` which includes only those rows for which `"mths_remng"` is empty, (2) create the DF `MRnulls.ByServ` which consists of the number of observations in `df_MRnull` grouped by `"servicer_name"` and (3) collect `MRnulls.ByServ` into a nicely formatted table as a local data.frame:
+df_MRnull <- where(df, isNull(df$mths_remng))
+MRnulls.ByServ <- agg(groupBy(df_MRnull, df_MRnull$servicer_name), Nulls = n(df_MRnull$servicer_name))
+MRnulls.ByServ.tab <- collect(MRnulls.ByServ)
+MRnulls.ByServ.tab
+
 
 # dropna
 
@@ -27,12 +46,6 @@ cache(df)
 
 # fillna
 # ifelse
-
-# isNaN(x)
-# isNull(x)
-# isNotNull(x)
-
-
 
 
 
