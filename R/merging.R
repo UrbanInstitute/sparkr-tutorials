b3d354a5229c26919a2096d4cec536cdb421c6c9
Armstrong <SArmstrong@urban.org>
2016-07-07 17:39:39 -0400

    Outlined merging.R tutorial

--- R/merging.R
+++ R/merging.R
@@ -18,3 +18,24 @@ sqlContext <- sparkRSQL.init(sc)
 
 df <- read.df(sqlContext, "s3://sparkr-tutorials/hfpc_ex", header="false", inferSchema="true", nullValue="")
 cache(df)
+
+# Join two DataFrames based on the given join expression.
+# join(x, y, joinExpr = NULL, joinType = NULL)
+# The type of join to perform. The following join types are available: 'inner', 'outer', 'full', 'fullouter', leftouter', 'left_outer', 'left', 'right_outer', 'rightouter', 'right', and 'leftsemi'. The default joinType is "inner".
+join(df1, df2) # Performs a Cartesian
+join(df1, df2, df1$col1 == df2$col2) # Performs an inner join based on expression
+join(df1, df2, df1$col1 == df2$col2, "right_outer")
+
+
+# Return a new DataFrame containing the union of rows in this DataFrame and another DataFrame. Note that this does not remove duplicate rows across the two DataFrames.
+# This is equivalent to 'UNION ALL' in SQL.
+unionAll(x, y)
+# Combines two (2) or more SparkR DataFrames by rows. Does not remove duplicate rows.
+rbind(x, y, z, w)
+
+
+# Check for duplicates prior to merging with `intersect`:
+intersect(x, y)
+
+# Return a new DF that includes only distinct rows, i.e. filters out duplicate rows that may result from merging:
+distinctDF <- distinct(df)
