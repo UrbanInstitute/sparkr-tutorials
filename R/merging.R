e6be3766d6b2b043e37a503b42e0b7d15d0d0105
Armstrong <SArmstrong@urban.org>
2016-07-07 17:23:23 -0400

    Started merging tutorial.R

---
+++ R/merging.R
@@ -0,0 +1,20 @@
+library(SparkR)
+
+## Initiate SparkContext:
+
+sc <- sparkR.init(sparkEnvir=list(spark.executor.memory="2g", 
+                                  spark.driver.memory="1g",
+                                  spark.driver.maxResultSize="1g")
+                  ,sparkPackages="com.databricks:spark-csv_2.11:1.4.0") # Load CSV Spark Package
+
+## AWS EMR is using Spark 2.11 so we need the associated version of spark-csv: http://spark-packages.org/package/databricks/spark-csv
+## Define Spark executor memory, as well as driver memory and maxResultSize according to cluster configuration
+
+## Initiate SparkRSQL:
+
+sqlContext <- sparkRSQL.init(sc)
+
+### Specify null values when loading data:
+
+df <- read.df(sqlContext, "s3://sparkr-tutorials/hfpc_ex", header="false", inferSchema="true", nullValue="")
+cache(df)
