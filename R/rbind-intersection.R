fb563100b54f29ba9de9c4913addb27cdde5cb59
Armstrong <SArmstrong@urban.org>
2016-07-14 18:34:30 -0400

    Uploaded rbind-intersection function as a .R file

---
+++ R/rbind-intersection.R
@@ -0,0 +1,15 @@
+##############################
+## rbind.intersect Function ##
+##############################
+# Sarah Armstrong, Urban Institute
+# July 14, 2016
+
+# Summary: Function that allows us to append rows of one SparkR DataFrame (DF) to another, regardless of the column names for each DF. Takes simple intersection of lists of column names and performs `rbind` SparkR operation on two (2) DFs, considering only the column names included in the intersected list of names.
+
+# Inputs: x (a DF) and y (another DF)
+# Returns: DataFrame
+
+rbind.intersect <- function(x, y) {
+  cols <- base::intersect(colnames(x), colnames(y))
+  return(SparkR::rbind(x[, cols], y[, cols]))
+}
