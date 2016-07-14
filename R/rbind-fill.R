6c5fb137998e9693c68e315c60eb2ad07a9960e9
Armstrong <SArmstrong@urban.org>
2016-07-14 16:59:40 -0400

    Upload rbind.fill function that let's us merge DF rows with different ncol values - fills missing entries with NA

---
+++ R/rbind-fill.R
@@ -0,0 +1,31 @@
+rbind.fill <- function(x, y) {
+
+  m1 <- ncol(x)
+  m2 <- ncol(y)
+  col_x <- colnames(x)
+  col_y <- colnames(y)
+  
+  if (m2 < m1) {
+    col_ <- list(setdiff(col_x, col_y), setdiff(col_y, col_x))
+    len <- length(col_)
+    for (j in 1:len){
+      y <- withColumn(y, col_[[j]], lit(NA))
+    }
+  } else { 
+    if (m2 == m1) {
+      col_ <- list(setdiff(col_x, col_y), setdiff(col_y, col_x))
+      len <- length(col_)
+      for (j in 1:len){
+        x <- withColumn(x, col_[[j]], lit(NA))
+        y <- withColumn(y, col_[[j]], lit(NA))
+      }
+    } else {
+      col_ <- list(setdiff(col_x, col_y), setdiff(col_y, col_x))
+      len <- length(col_)
+      for (j in 1:len){
+          x <- withColumn(x, col_[[j]], lit(NA))
+      }
+    }         
+  }
+  return(SparkR::rbind(x, y))
+}
