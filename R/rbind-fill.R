921c1c5dac07d18f0b57916d374eebf6629b37c2
Armstrong <SArmstrong@urban.org>
2016-07-15 11:38:50 -0400

    Fixed bug in rbind.fill function & tweaked to make faster

--- R/rbind-fill.R
+++ R/rbind-fill.R
@@ -10,33 +10,31 @@
 # Returns: DataFrame
 
 rbind.fill <- function(x, y) {
-
+  
   m1 <- ncol(x)
   m2 <- ncol(y)
   col_x <- colnames(x)
   col_y <- colnames(y)
+  outersect <- function(x, y) {setdiff(union(x, y), intersect(x, y))}
+  col_ <- outersect(col_x, col_y)
+  len <- length(col_)
   
   if (m2 < m1) {
-    col_ <- list(setdiff(col_x, col_y), setdiff(col_y, col_x))
-    len <- length(col_)
     for (j in 1:len){
       y <- withColumn(y, col_[[j]], lit(NA))
     }
   } else { 
-    if (m2 == m1) {
-      col_ <- list(setdiff(col_x, col_y), setdiff(col_y, col_x))
-      len <- length(col_)
+    if (m2 > m1) {
+        for (j in 1:len){
+          x <- withColumn(x, col_[[j]], lit(NA))
+        }
+      }
+    if (m2 == m1 & col_x != col_y) {
       for (j in 1:len){
         x <- withColumn(x, col_[[j]], lit(NA))
         y <- withColumn(y, col_[[j]], lit(NA))
       }
-    } else {
-      col_ <- list(setdiff(col_x, col_y), setdiff(col_y, col_x))
-      len <- length(col_)
-      for (j in 1:len){
-          x <- withColumn(x, col_[[j]], lit(NA))
-      }
-    }         
+    } else {}         
   }
   return(SparkR::rbind(x, y))
 }
