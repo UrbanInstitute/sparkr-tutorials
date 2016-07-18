20a52b9142c67944a608e166f9fa4d6993cd733c
Armstrong <SArmstrong@urban.org>
2016-07-18 14:47:30 -0400

    Added sort column name specification to rbind.intersection

--- R/rbind-intersection.R
+++ R/rbind-intersection.R
@@ -11,5 +11,5 @@
 
 rbind.intersect <- function(x, y) {
   cols <- base::intersect(colnames(x), colnames(y))
-  return(SparkR::rbind(x[, cols], y[, cols]))
+  return(SparkR::rbind(x[, sort(colnames(x))], y[, sort(colnames(y))]))
 }
