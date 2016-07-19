ff88705235d6751b1830194fbf19a749eab75394
Armstrong <SArmstrong@urban.org>
2016-07-19 09:55:29 -0400

    Added devtools and ggplot.SparkR install in preamble

--- R/visualization.R
+++ R/visualization.R
@@ -1,6 +1,9 @@
-library(SparkR)
+install.packages('devtools')
+library(devtools)
 devtools::install_github("SKKU-SKT/ggplot2.SparkR")
+library(SparkR)
 library(ggplot2.SparkR)
+
 sc <- sparkR.init(sparkEnvir=list(spark.executor.memory="2g", spark.driver.memory="1g", spark.driver.maxResultSize="1g"), sparkPackages="com.databricks:spark-csv_2.11:1.4.0")
 sqlContext <- sparkRSQL.init(sc)
 
@@ -9,6 +12,8 @@ sqlContext <- sparkRSQL.init(sc)
 df <- read.df(sqlContext, "s3://ui-spark-data/diamonds.csv", header='true', delimiter=",", source="com.databricks.spark.csv", inferSchema='true', nullValue="")
 cache(df)
 head(df)
+
+
 
 ### Bar graph:
 
