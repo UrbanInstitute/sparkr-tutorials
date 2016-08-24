72a153faf2c6c9590250db9129b8778e0035953b
Armstrong <SArmstrong@urban.org>
2016-08-24 16:03:48 -0400

    Changed qqnorm file in glm tutorial to reflect standardized residual qqnorm function change

--- R/glm.R
+++ R/glm.R
@@ -142,36 +142,37 @@ summary(glm.poisson)
 
 # Fitted v. Residual Values plot
 
+#  Detect non-linearity, unequal error variances, and outliers
+
 p6 <- geom_bivar_histogram.SparkR(df = df, x = "y_hat", y = "res", nbins = 250)
 p6 + scale_colour_brewer(palette = "Blues", type = "seq") + xlab("Fitted Value") + ylab("Residual") + 
   ggtitle("Fitted v. Residual Values")
 
 # Q-Q plot of the residuals
 
-qqres_plot.SparkR <- function(df, residuals, qn = 100, error){
+# Detect non-normality of the residuals
+
+qqnorm.SparkR <- function(df, residuals, qn = 100, error){
   
   resdf <- select(df, residuals)
   
-  n <- nrow(resdf)
-  mean.res <- collect(agg(resdf, avg(resdf[[residuals]])))[[1]]
-  var.res <- collect(agg(resdf, var(resdf[[residuals]])))[[1]]
   sd.res <- collect(agg(resdf, stddev(resdf[[residuals]])))[[1]]
-  min.res <- collect(agg(resdf, min(resdf[[residuals]])))[[1]]
-  max.res <- collect(agg(resdf, max(resdf[[residuals]])))[[1]]
+  
+  resdf <- withColumn(resdf, "stdres", resdf[[residuals]] / sd.res)
   
   probs <- seq(0, 1, length = qn)
   
-  norm_quantiles <- qnorm(probs, mean = mean.res, sd = sd.res)
-  res_quantiles <- unlist(approxQuantile(resdf, col = residuals, probabilities = probs, relativeError = error))
+  norm_quantiles <- qnorm(probs, mean = 0, sd = 1)
+  stdres_quantiles <- unlist(approxQuantile(resdf, col = "stdres", probabilities = probs, relativeError = error))
   
-  dat <- data.frame(sort(norm_quantiles), sort(res_quantiles))
+  dat <- data.frame(sort(norm_quantiles), sort(stdres_quantiles))
   
-  p <- ggplot(dat, aes(norm_quantiles, res_quantiles))
+  p <- ggplot(dat, aes(norm_quantiles, stdres_quantiles))
   
-  p + geom_point(color = "#FF3333") + geom_abline(intercept = 0, slope = 1) + xlab("Theoretical Quantiles") + ylab("Sample Quantiles") + geom_hline(aes(yintercept = min(dat$sort.res_quantiles.), linetype = "Residual Extremum Values"), show.legend = TRUE) + geom_hline(yintercept = max(dat$sort.res_quantiles.), linetype = "dotted") + scale_linetype_manual(values = c(name = "none", "Residual Extremum Values" = "dotted")) + guides(linetype = guide_legend("")) + theme(legend.position = "bottom")
+  p + geom_point(color = "#FF3333") + geom_abline(intercept = 0, slope = 1) + xlab("Normal Scores") + ylab("Standardized Residuals") + geom_hline(aes(yintercept = min(dat$sort.stdres_quantiles.), linetype = "Residual Extremum Values"), show.legend = TRUE) + geom_hline(yintercept = max(dat$sort.stdres_quantiles.), linetype = "dotted") + scale_linetype_manual(values = c(name = "none", "Residual Extremum Values" = "dotted")) + guides(linetype = guide_legend("")) + theme(legend.position = "bottom")
   
 }
 
-p7 <- qqres_plot.SparkR(df = df, residuals = "res", qn = 100, error = 0.0001)
+p7 <- qqnorm.SparkR(df = df, residuals = "res", qn = 100, error = 0.0001)
 p7 + ggtitle("This is a title")
 
